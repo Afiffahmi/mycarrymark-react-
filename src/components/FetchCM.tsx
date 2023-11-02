591ba865bf475da3cb743c59982baec2ff6106c5
Afif Fahmi <afif.rafpi@gmail.com>
2023-11-02 22:34:02 +0800

    Update FetchCM.tsx

--- src/components/FetchCM.tsx
+++ src/components/FetchCM.tsx
@@ -16,9 +16,12 @@ import Slider from "@mui/joy/Slider";
 import ListDivider from '@mui/joy/ListDivider';
 import Avatar from '@mui/joy/Avatar';
 import List from '@mui/joy/List';
-import { Alert, Chip, ListItem, ListItemDecorator } from "@mui/joy";
+import { Alert, CardOverflow, Chip, ListItem, ListItemDecorator, Tooltip } from "@mui/joy";
 import ListItemContent from "@mui/joy/ListItemContent";
 import AvatarGroup from '@mui/joy/AvatarGroup';
+import Badge, { badgeClasses } from '@mui/joy/Badge';
+import { BarChart } from "@mui/x-charts/BarChart";
+import {createTheme} from '@mui/material/styles'
 
 const marks = [
   {
@@ -102,11 +105,13 @@ export default function MyProfile() {
             </Stack>
           </Stack>
         </Card>
-        <Card>
+        <Card variant="outlined" color="neutral">
           <Box sx={{ mb: 1 }}>
             <Typography level="title-md">Assignment Submmission</Typography>
             <Typography level="body-sm">
-              Students turned in the assignment
+              Students <Typography variant="solid" color="success" noWrap>
+    turned in
+  </Typography> the assignment
             </Typography>
           </Box>
           <Divider />
@@ -156,14 +161,121 @@ export default function MyProfile() {
           
          
         </ListItem>
-         <AvatarGroup size="sm" sx={{ flexDirection: 'row-reverse' }}>
+        <Box height={80}></Box>
+         <ListItem>
+          <ListItemContent>
+          <AvatarGroup size="sm" sx={{ flexDirection: 'row-reverse' }}>
       <Avatar>+3</Avatar>
       <Avatar alt="Cindy Baker" src="https://cdn.uitm.edu.my/gambar_warga/92f42034219a8d85cac7c21169e8b1c4.png" />
       <Avatar alt="Travis Howard" src="https://cdn.uitm.edu.my/gambar_warga/f4251db8ceea3f3f2eb6a79bfc4d8282.png" />
       <Avatar alt="Remy Sharp" src="https://cdn.uitm.edu.my/gambar_warga/c4db5493fa57464b8c9e020aec691ab0.png" />
     </AvatarGroup>
+          </ListItemContent>
+         </ListItem>
       </List>
     </Box>
+          </Stack>
+        </Card>
+        
+
+        <Card variant="outlined" color="danger">
+          
+          <Box sx={{ mb: 1 }}>
+            <Typography level="title-md">Performance Monitoring</Typography>
+            <Typography level="body-sm">
+              Shown students currently in <Typography variant="solid" color="danger" noWrap>
+    Poor performance
+  </Typography>
+            </Typography>
+          </Box>
+          <Divider />
+          <Stack spacing={2} sx={{ my: 1 }}direction='row'>
+          <Box sx={{ width: 320 }}>
+      <Typography
+        id="ellipsis-list-demo"
+        level="body-xs"
+        textTransform="uppercase"
+        sx={{ letterSpacing: '0.15rem' }}
+      >
+        Students
+      </Typography>
+      <List aria-labelledby="ellipsis-list-demo"
+        sx={{ '--ListItemDecorator-size': '56px' }}>
+        <ListItem>
+          <ListItemDecorator>
+          <Badge
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        badgeInset="14%"
+        color="danger"
+        sx={{
+          [`& .${badgeClasses.badge}`]: {
+            '&::after': {
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              borderRadius: '50%',
+              animation: 'ripple 1.2s infinite ease-in-out',
+              border: '2px solid',
+              borderColor: 'danger.500',
+              content: '""',
+            },
+          },
+          '@keyframes ripple': {
+            '0%': {
+              transform: 'scale(1)',
+              opacity: 1,
+            },
+            '100%': {
+              transform: 'scale(2)',
+              opacity: 0,
+            },
+          },
+        }}
+      >
+        <Avatar alt="Remy Sharp" src="https://cdn.uitm.edu.my/gambar_warga/4a975a190f7459aaf9ecd73fa5ab5f02.png" />
+      </Badge>
+          </ListItemDecorator>
+          <ListItemContent>
+          <Typography level="title-sm">Muhammad Afif Fahmi</Typography>
+            <Stack direction='row'>
+            <Typography level="body-sm" noWrap>
+              Current Assesment Score 
+            </Typography>
+            <Chip color="danger" size="sm">6/15</Chip>
+            </Stack>
+          </ListItemContent>
+        </ListItem>
+        <ListItem>
+          <ListItemDecorator>
+          <Tooltip title="Quiz 1" variant="soft">
+            <Button variant="soft" color="danger">3/5</Button>
+          </Tooltip>
+          </ListItemDecorator>
+          <Box height={40}></Box>
+          <ListItemContent>
+        
+          <Typography>Quiz 1</Typography>
+        
+          </ListItemContent>
+        </ListItem>
+        <ListItem>
+          <ListItemDecorator>
+          <Tooltip title="Quiz 1" variant="soft">
+            <Button variant="soft" color="danger">3/10</Button>
+          </Tooltip>
+          </ListItemDecorator>
+          <Box height={40}></Box>
+          <ListItemContent>
+        
+          <Typography>Test 1</Typography>
+        
+          </ListItemContent>
+        </ListItem>
+      </List>
+    </Box>
+
           </Stack>
         </Card>
       </Stack>
